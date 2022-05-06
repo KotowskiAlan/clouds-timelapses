5e30c4ecbbf3ac69d2bcbcc28b41ff6f3401d9e5
bartlomiejstrz <bartek.strzelczyk@optilyz.com>
2022-05-06 17:34:46 +0200

    small code style updates

--- script.js
+++ script.js
@@ -15,68 +15,64 @@ const timelapseTriangle = document.querySelector('.timelapseTriangle');
 timelapseDIYList.addEventListener('click', ()=> {
     timelapseDIY.classList.toggle('timelapseDIYOn');
     timelapseTriangle.classList.toggle('timelapseTriangleOn');
-   
 })
 
 // WINDOWS EASE-IN & OUT
 
-const elements = document.querySelectorAll(".easing");
+const easingElements = document.querySelectorAll(".easing");
 
 const onScroll = () => {
     const yScroll = window.pageYOffset;
     const bodyRect = document.body.getBoundingClientRect();
 
-    elements.forEach((el) => {
+    easingElements.forEach((el) => {
       const cloudRect = el.getBoundingClientRect();
       const windowPosition = cloudRect.top - bodyRect.top - 200;
 
-        el = yScroll >= windowPosition ? el.classList.add('easingOn') : el.classList.remove('easingOn');
+      yScroll >= windowPosition ? el.classList.add('easingOn') : el.classList.remove('easingOn');
     })
 }
 
 window.addEventListener("scroll", onScroll)
 
-// TIMELAPSE GALERY
+// TIMELAPSE GALLERY
 
 let actualTimelapse = document.querySelector('.actualTimelapse');
 
-
 const video = document.createElement("VIDEO");
-video.controls = "controls", "muted";
+video.controls = "controls";
+video.muted = "muted";
 actualTimelapse.appendChild(video);
-let timelapse = document.querySelectorAll('.timelapse');
-actualThumbnail = timelapse[0];
+let timelapses = document.querySelectorAll('.timelapse');
+actualThumbnail = timelapses[0];
 video.src = actualThumbnail.getAttribute('src');
 
-for (let i = 0; i < timelapse.length; i++) {
-    timelapse[i].onclick = function() {
-        video.src = this.getAttribute('src');
+for (let i = 0; i < timelapses.length; i++) {
+    timelapses[i].onclick = () => {
+      video.src = timelapses[i].getAttribute('src');
     }
-    timelapse[i].onmouseover = function() {
-        timelapse[i].play();
-       
-        
+
+    timelapses[i].onmouseover = function() {
+        timelapses[i].play();
     }
-    timelapse[i].onmouseout = function() {
-        timelapse[i].pause();      
+    timelapses[i].onmouseout = function() {
+        timelapses[i].pause();
     }
 }
 
 // TIMELAPSE GALERY CAROUSEL
-
 const previousTimelapse = document.querySelector('.previousTimelapse');
 const nextTimelapse = document.querySelector('.nextTimelapse');
-const timelapses = document.querySelector('.timelapses');
+const timelapsesCarouselElements = document.querySelector('.timelapses');
+
 previousTimelapse.addEventListener('click', () => {
-    
-    timelapses.classList.remove('timelapsesOn');
+    timelapsesCarouselElements.classList.remove('timelapsesOn');
     previousTimelapse.classList.add('previousTimelapseOn');
     nextTimelapse.classList.remove('nextTimelapseOn');
 })
 
 nextTimelapse.addEventListener('click', () => {
-    
-    timelapses.classList.add('timelapsesOn');
+    timelapsesCarouselElements.classList.add('timelapsesOn');
     nextTimelapse.classList.add('nextTimelapseOn');
     previousTimelapse.classList.remove('previousTimelapseOn');
 })
